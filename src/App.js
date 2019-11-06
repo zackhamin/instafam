bbca1617d3f57fdaa87b9a8449ab9a91b16bf6cd
Zack Amin <ishaqhamin@gmail.com>
2019-11-06 12:06:56 +0000

    footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import BottomBar from './Components/BottomBar'
+
+function App() {
+    return (
+        <div>
+            <BottomBar />
+        </div>
+    )
+}
+
+
+
+
+export default App;
---
+++ src/Components/BottomBar.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+
+
+
+export default class BottomBar extends Component {
+   render() {
+       return (
+           <div className="BottomBar">
+
+
+                <div class="logos">
+      <div class="img_wrap">
+      <i class="fas fa-home icon"></i>
+      </div>
+      <div class="img_wrap">
+      <i class="fas fa-search icon"></i>
+      </div>
+      <div class="img_wrap">
+      <i class="far fa-plus-square icon"></i>
+      </div> 
+      <div class="img_like">
+      <i class="far fa-heart icon"></i>
+      </div>
+      <div class="img_wrap">
+      <i class="far fa-user icon"></i>
+      </div>
+              
+        </div>
+        </div>
+           
+       )
+   }
+}
