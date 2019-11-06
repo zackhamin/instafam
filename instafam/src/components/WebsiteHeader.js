b21480babde9a0d6d221da1a47e2f7a5071cc711
james-poolie <56539323+james-poolie@users.noreply.github.com>
2019-11-06 10:03:42 +0000

    folders created

---
+++ instafam/src/components/WebsiteHeader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WebsiteHeader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
