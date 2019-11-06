86c983837bf3b3af37dcd9fca91b239bb68c07ce
james-poolie <56539323+james-poolie@users.noreply.github.com>
2019-11-06 13:44:35 +0000

    Progress with headers and message page

--- instafam/src/App.js
+++ instafam/src/App.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Messages from './components/Messages';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path='/' render={() =>
+                            <div>
+                                <Navigation />
+                                <Home />
+                            </div>
+                        } />
+                        <Route exact path='/Messages' render={() =>
+                            <Messages />
+                        } />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
 
 export default App;
