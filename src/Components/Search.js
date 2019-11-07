e3e86767a8f4a2c1cb9da66fcaf637098fcfa56b
Zack Amin <ishaqhamin@gmail.com>
2019-11-07 10:57:23 +0000

    finished

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,6 +5,29 @@ import meme3 from '../Images/meme3.png';
 import meme4 from '../Images/meme4.png';
 import meme5 from '../Images/meme5.png';
 import meme6 from '../Images/meme6.png';
+import meme7 from '../Images/meme7.png';
+import meme8 from '../Images/meme8.png';
+import meme9 from '../Images/meme9.png';
+import meme10 from '../Images/meme10.png';
+import meme11 from '../Images/meme11.png';
+import meme12 from '../Images/meme12.png';
+import meme13 from '../Images/meme13.png';
+import meme14 from '../Images/meme14.png';
+import meme15 from '../Images/meme15.png';
+import meme16 from '../Images/meme16.png';
+import meme17 from '../Images/meme17.png';
+import meme18 from '../Images/meme18.png';
+import meme19 from '../Images/meme19.png';
+import meme20 from '../Images/meme20.png';
+import meme21 from '../Images/meme21.png';
+import meme22 from '../Images/meme22.jpg';
+import meme23 from '../Images/meme23.jpeg';
+import meme24 from '../Images/meme24.jpg';
+import meme25 from '../Images/meme25.jpg';
+import meme26 from '../Images/meme26.jpg';
+import meme27 from '../Images/meme27.jpg';
+
+
 
 
 
@@ -33,6 +56,72 @@ export default class Search extends Component {
       </div>
       <div className="img_search">
         <img src= {meme6} alt="6th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme7} alt="7th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme8} alt="8th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme9} alt="9th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme10} alt="10th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme11} alt="11th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme12} alt="12th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme13} alt="13th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme14} alt="14th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme15} alt="15th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme16} alt="16th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme17} alt="17th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme18} alt="18th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme19} alt="19th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme20} alt="20th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme21} alt="21th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme22} alt="22th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme23} alt="23th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme24} alt="24th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme25} alt="25th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme26} alt="26th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme26} alt="26th"/>
+      </div>
+      <div className="img_search">
+        <img src= {meme27} alt="27th"/>
       </div>
   </div>     
     )
